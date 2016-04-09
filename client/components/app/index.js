d98330f3daca4fc3d06d764681872c3e83586fd5
Ray Luong <rayluong@gmail.com>
2016-04-09 10:07:37 -0700

    webpack setup

---
+++ client/components/app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {} from './style.less';
+
+class App extends React.Component {
+	render() {
+		return <div>Hello world from a React component</div>;
+	}
+}
+
+export default App;
