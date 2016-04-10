6ad79c1695c9d341d6b1044dd99cfe698e4c0cc6
Ray Luong <rayluong@gmail.com>
2016-04-10 14:34:54 -0700

    express, react, babel, and hbs setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
+import path from 'path';
 import express from 'express';
+import handlebars from 'express-handlebars';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import App from './generated/app';
+
 const app = express();
 
-app.get('/', (request,response) => {
-	response.send('Hello world from Express');
+// View templates
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Static assets
+app.use(express.static(path.resolve(__dirname, './dist')));
+
+// Routes
+app.get('/', (request, response) => {
+  response.render('app', {
+    app: ReactDOMServer.renderToString(<App />)
+  });
 });
 
-app.listen(3000, () => console.log('Server running at 3000...'));
+app.listen(3000, () => console.log('Server running'));
